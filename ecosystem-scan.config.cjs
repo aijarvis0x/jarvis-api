9d1b4542fb99b979177d1f1b062ac887b1f232ed
vxhuy <vxhuy17694@gmail.com>
2025-03-06 17:47:19 +0700

    update

--- ecosystem-scan.config.cjs
+++ ecosystem-scan.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "scan",
-      script: "dist/scan/start.js",
+      script: "dist/scan/scan.js",
       node_args: [],
       max_memory_restart: "4000M",
       instances: "1",
