df5374a2f7c9e79d121c483d9097c116f888e242
Hieucaohd <hieucaohd@gmail.com>
2025-03-25 13:50:38 +0700

    update

--- src/schemas/explore.schema.ts
+++ src/schemas/explore.schema.ts
@@ -1,14 +1,30 @@
 import { z } from "zod"
 import { paginationSchema } from "./generic-schemas.js";
+import { string } from "pg-format";
 
 export const addFavoriteBotBody = z.object({
 	botId: z.string()
 });
 
+export enum TimeFilterTrend {
+	All = "all",
+	OneHour = "1h",
+	SixHours = "6h",
+	TwentyHours = "20h",
+	SevenDays = "7d"
+}
+
+export enum TrendType {
+	Trending = "trending",
+	Top = "Top",
+	PriceDesc = "PriceDesc"
+}
+
 export const listTrendingBotQuery = {
 	querystring: paginationSchema.merge(
 		z.object({
-			timeTrend: z.string().optional(),
+			timeFilterTrend: z.enum(Object.values(TimeFilterTrend) as [string, ...string[]]).default(TimeFilterTrend.All).optional(),
+			trendType: z.enum(Object.values(TrendType) as [string, ...string[]]).optional().default(TrendType.Trending),
 		})
 	),
 } as const
