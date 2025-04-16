8c02d3b00f6b63dd93bf4f2bae93514d57438d3c
Hieucaohd <hieucaohd@gmail.com>
2025-04-16 22:40:57 +0700

    update

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -42,7 +42,7 @@ export default async (app: AppInstance) => {
             body: loginSchema,
         },
         handler: async (request, reply) => {
-            const { address, signature, messageHash, userRefId } = request.body;
+            const { address, signature, messageHash, userRefCode } = request.body;
 
             try {
                 //verify signature
@@ -59,7 +59,7 @@ export default async (app: AppInstance) => {
                 }
 
                 //login -> gen token
-                let userData = await login(address, userRefId);
+                let userData = await login(address, userRefCode);
 
                 return reply.status(200).send({
                     message: "OK",
